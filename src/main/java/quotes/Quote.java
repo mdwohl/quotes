fede9f9fa3083c90654bde162fda4ac704cc722c
mdwohl <mdwohl@gmail.com>
2020-09-23 16:09:49 -0700

    lab08 setup

---
+++ src/main/java/quotes/Quote.java
@@ -0,0 +1,23 @@
+package quotes;
+
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Quote {
+    String quoteBody;
+    String author;
+    Gson gson = new Gson();
+
+    public Quote(String quoteBody, String author){
+        this.quoteBody = quoteBody;
+        this.author = author;
+    }
+
+    public String toString(){
+        return String.format("' %s ' -%s");
+    }
+}
+
